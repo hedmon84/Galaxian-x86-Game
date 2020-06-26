b81677142f60a49bf26bc2a41fbef07a290089cd
Hedmon Lopez <hedmonlopes@gmail.com>
2020-06-25 23:40:11 -0600

    more enemies added

--- Galaxian_Game.asm
+++ Galaxian_Game.asm
@@ -1,7 +1,21 @@
 
 ; 0x0a030a04 first colum enemies
 start:
-    
+
+    push 55   ;enemy8 der 4
+    push 15
+    push 51   ;enemy7 der 3
+    push 15
+    push 27   ;enemy6 iz 3
+    push 15
+    push 47   ;enemy5 der 2
+    push 15
+    push 31   ;enemy4 iz 2
+    push 15
+    push 43   ;enemy3 der 1
+    push 15
+    push 35   ;enemy2 iz1
+    push 15
     push 39   ;enemy1
     push 15
     push 39   ;shot
@@ -18,6 +32,10 @@ start:
 ; dword [ebp+20] => Colum
 ; dword [ebp+ 24] => Row ;enemy1
 ; dword [ebp+28] => Colum
+; dword [ebp+ 32] => Row ;enemy2
+; dword [ebp+36] => Colum
+; dword [ebp+ 40] => Row ;enemy3
+; dword [ebp+44] => Colum
 start:
     ;epilogo
     push ebp
@@ -34,18 +52,99 @@ start:
    mov dword [ebp-8], 0x04000401  ; current shot
    call offset_shot
    mov dword[0x10000004], edi
-    ; ----------------- load paint enemy1------------
+    ; ----------------- load paint enemy1------------ works middle
     mov dword[0x10000024],0  ; death flag
     sub esp,4
-    mov dword [ebp-12],0x0a030a04
+    mov dword [ebp-12],0x05030504
     call offset_enemy1
     #show dword[0x10000020]
+    ;-----------------load paint enemy2---------------- ; falta morir
+    mov dword[0x10000032],0  ; death flag
+    sub esp,4
+    mov dword [ebp-16],0x0a030a04
+    call offset_enemy2
+    #show dword[0x10000028]
 
-   ; -------------paint enemy1-------------
-    mov eax,dword[0x10000020]
-    mov ebx, dword[ebp-12]
+    ;-----------------print enemy2 test-----------------  
+
+    mov eax,dword[0x10000028]
+    mov ebx, dword[ebp-16]
     mov dword[eax],ebx
+    ;-----------------load paint enemy3---------------- ; falta morir
+    mov dword[0x10000046],0  ; death flag
+    sub esp,4
+    mov dword [ebp-20],0x0a030a04
+    call offset_enemy3
+     #show dword[0x10000036]
     
+    ;-----------------print enemy3 test-----------------  
+    mov eax,dword[0x10000036]
+    mov ebx, dword[ebp-20]
+    mov dword[eax],ebx
+
+    ;-----------------load paint enemy4---------------- ; falta morir
+    mov dword[0x10000050],0  ; death flag
+    sub esp,4
+    mov dword [ebp-24],0x0a030a04
+    call offset_enemy4
+     #show dword[0x10000054]
+    
+    ;-----------------print enemy4 test-----------------  
+    mov eax,dword[0x10000054]
+    mov ebx, dword[ebp-24]
+    mov dword[eax],ebx
+
+    ;-----------------load paint enemy5---------------- ; falta morir
+    mov dword[0x10000058],0  ; death flag
+    sub esp,4
+    mov dword [ebp-28],0x0a030a04
+    call offset_enemy5
+     #show dword[0x10000062]
+    
+    ;-----------------print enemy5 test-----------------  
+    mov eax,dword[0x10000062]
+    mov ebx, dword[ebp-28]
+    mov dword[eax],ebx
+
+    ;-----------------load paint enemy6---------------- ; falta morir
+    mov dword[0x10000062],0  ; death flag
+    sub esp,4
+    mov dword [ebp-32],0x0a030a04
+    call offset_enemy6
+     #show dword[0x10000066]
+    
+    ;-----------------print enemy6 test-----------------  
+    mov eax,dword[0x10000066]
+    mov ebx, dword[ebp-32]
+    mov dword[eax],ebx
+
+    ;-----------------load paint enemy7---------------- ; falta morir
+    mov dword[0x10000070],0  ; death flag
+    sub esp,4
+    mov dword [ebp-36],0x0a030a04
+    call offset_enemy7
+     #show dword[0x10000074]
+    
+    ;-----------------print enemy7 test-----------------  
+    mov eax,dword[0x10000074]
+    mov ebx, dword[ebp-36]
+    mov dword[eax],ebx
+
+    ;-----------------load paint enemy8---------------- ; falta morir
+    mov dword[0x10000078],0  ; death flag
+    sub esp,4
+    mov dword [ebp-40],0x0a030a04
+    call offset_enemy8
+     #show dword[0x10000082]
+    
+    ;-----------------print enemy8 test-----------------  
+    mov eax,dword[0x10000082]
+    mov ebx, dword[ebp-40]
+    mov dword[eax],ebx
+
+
+
+
 
 
 $Game_loop:
@@ -62,7 +161,7 @@ $Game_loop:
     mov ebx,dword[ebp-8]
     mov dword[edi],ebx
     call shot
-;------------------paint enemy1--------------------
+;------------------paint enemy1 death*--------------------
 
        cmp dword[0x10000024],1
        je death1
@@ -177,7 +276,7 @@ $delay_loop:
     cmp dword [0xffff0008], eax
     jl $delay_loop
     ret
-
+;---------------------------------offsets zone------------------------------
 offset:
 mov esi, dword [ebp+8] ; Row
     mov ebx, esi
@@ -211,11 +310,90 @@ mov eax, dword [ebp+24] ; Row
     add eax, 0xb800
     mov dword[0x10000020] , eax
     ret
+offset_enemy2:
+mov eax, dword [ebp+32] ; Row
+    mov ebx, eax
+    shl eax, 6
+    shl ebx, 4
+    add eax, ebx
+    add eax, dword [ebp+36] ; Col
+    shl eax, 1
+    add eax, 0xb800
+    mov dword[0x10000028] , eax
+    ret
 
+offset_enemy3:
+mov eax, dword [ebp+40] ; Row
+    mov ebx, eax
+    shl eax, 6
+    shl ebx, 4
+    add eax, ebx
+    add eax, dword [ebp+44] ; Col
+    shl eax, 1
+    add eax, 0xb800
+    mov dword[0x10000036] , eax
+    ret
+offset_enemy4:
+mov eax, dword [ebp+48] ; Row
+    mov ebx, eax
+    shl eax, 6
+    shl ebx, 4
+    add eax, ebx
+    add eax, dword [ebp+52] ; Col
+    shl eax, 1
+    add eax, 0xb800
+    mov dword[0x10000054] , eax
+    ret
 
+offset_enemy5:
+mov eax, dword [ebp+56] ; Row
+    mov ebx, eax
+    shl eax, 6
+    shl ebx, 4
+    add eax, ebx
+    add eax, dword [ebp+60] ; Col
+    shl eax, 1
+    add eax, 0xb800
+    mov dword[0x10000062] , eax
+    ret
+offset_enemy6:
+mov eax, dword [ebp+64] ; Row
+    mov ebx, eax
+    shl eax, 6
+    shl ebx, 4
+    add eax, ebx
+    add eax, dword [ebp+68] ; Col
+    shl eax, 1
+    add eax, 0xb800
+    mov dword[0x10000066] , eax
+    ret
 
+offset_enemy7:
+mov eax, dword [ebp+72] ; Row
+    mov ebx, eax
+    shl eax, 6
+    shl ebx, 4
+    add eax, ebx
+    add eax, dword [ebp+76] ; Col
+    shl eax, 1
+    add eax, 0xb800
+    mov dword[0x10000074] , eax
+    ret
+
+offset_enemy8:
+mov eax, dword [ebp+80] ; Row
+    mov ebx, eax
+    shl eax, 6
+    shl ebx, 4
+    add eax, ebx
+    add eax, dword [ebp+84] ; Col
+    shl eax, 1
+    add eax, 0xb800
+    mov dword[0x10000082] , eax
+    ret
+
+;---------------------------------------gun zone----------------------------------
 shot:
-
 
 mov ecx,dword[0x10000012]
 cmp dword[0x10000008],1
